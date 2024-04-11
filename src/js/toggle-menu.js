e4fb765de173767a6b6df1f5e9aaada7207ca99a
Nell <eemandersson@outlook.com>
2024-04-11 08:35:40 +0200

    added span elements in html, toggle menu js, removed hover effect and added active state

--- src/js/toggle-menu.js
+++ src/js/toggle-menu.js
@@ -4,3 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.toggle('menu-active', this.checked);
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var links = document.querySelectorAll('.menu li a');
+
+  links.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      links.forEach(function (lnk) {
+        lnk.classList.remove('active');
+      });
+
+      this.classList.add('active');
+    });
+  });
+});

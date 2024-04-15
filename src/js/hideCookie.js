64b83d43d857f10d728c9fd1f9e556eecd4c970c
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-04-15 15:33:20 +0700

    Linking all image + fixing cookie btn
    added linking to all images + sound playing from middle image. + Fixing the ok btn in cookies.

--- src/js/hideCookie.js
+++ src/js/hideCookie.js
@@ -2,7 +2,7 @@ export default function hideCookieBar() {
   let bar = document.querySelector('.cookieBar');
   let okButton = document.querySelector('.cookieOkBtn');
 
-  okButton.addEventListener('click', function() {
+  okButton.addEventListener('click', function () {
     bar.style.display = 'none';
   });
 }

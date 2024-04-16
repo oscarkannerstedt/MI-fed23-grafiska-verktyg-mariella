7184de41f282b5ad601674644d827a9c4ff01993
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-04-16 13:33:42 +0200

    [Added] - id to html

---
+++ src/js/contactForm.js
@@ -0,0 +1,7 @@
+export default function disableButton() {
+    const form = document.getElementById('contactForm');
+    const firstnameInput = document.getElementById('firstnameInput');
+    const emailInput = document.getElementById('emailInput');
+    const checkBox1 = document.getElementById('checkBox1');
+    const submitButton = document.getElementById('submitButton');
+}
